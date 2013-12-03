564ea5826c7a54c78c35135f05845c5bee24d714
Erik Donohoo <erikdonohoo@gmail.com>
2013-12-02 20:17:16 -0700

    Fixed another bug on mobile where not able to submit income/expense

--- js/controllers/budget.js
+++ js/controllers/budget.js
@@ -191,6 +191,7 @@ angular.module("BudgetBuddy").controller('BudgetCtrl', function($q, $timeout, $l
 	$scope.addExpense = function() {
 		$scope.newExpense = {};
 		$scope.newExpense.fakeDate = $filter('date')($scope.now, 'yyyy-MM-dd');
+		$scope.newExpense.date = $scope.fixDate($scope.newExpense);
 	}
 	$scope.fixDate = function(expense) {
 		var stuff = expense.fakeDate.split("-");
@@ -235,6 +236,7 @@ angular.module("BudgetBuddy").controller('BudgetCtrl', function($q, $timeout, $l
 	$scope.addIncome = function() {
 		$scope.newIncome = {};
 		$scope.newIncome.fakeDate = $filter('date')($scope.now, 'yyyy-MM-dd');
+		$scope.newIncome.date = $scope.fixDate($scope.newIncome)
 	}
 	$scope.cancelIncome = function() {
 		$scope.newIncome = null;
